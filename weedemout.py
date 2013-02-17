2acc13b00391d20074449f7b02fee72eaf7486ba
Pranav Ravichandran <me@onloop.net>
2013-02-17 23:12:45 +0530

    Indentation trouble.

--- weedemout.py
+++ weedemout.py
@@ -6,61 +6,61 @@ import sys
 import socket
 
 def returnBlacklist(username):
-	followingList = {}
-	followedList = []
-	blackSheep = ''
-	pageNumber = 1
-	scrapeFollowing = True
-	scrapeFollowers = True
+    followingList = {}
+    followedList = []
+    blackSheep = ''
+    pageNumber = 1
+    scrapeFollowing = True
+    scrapeFollowers = True
 
-	while True:
-		try:
-			if scrapeFollowing:
-				followingPage = urllib2.urlopen("http://soundcloud.com/" + username + "/following?page=" + str(pageNumber), timeout = 3).read()
-			if scrapeFollowers:
-				followersPage = urllib2.urlopen("http://soundcloud.com/" + username + "/followers?page=" + str(pageNumber), timeout = 3).read()
-		except urllib2.HTTPError, e:
-	        	return 'User not found.'
-		except urllib2.URLError, e:
-			if pageNumber > 1:
-				break
-			else:
-				if isinstance(e.reason, socket.timeout):
-					return 'Timed out.'
-				return 'Bummer, failed to connect :('
+    while True:
+        try:
+            if scrapeFollowing:
+                followingPage = urllib2.urlopen("http://soundcloud.com/" + username + "/following?page=" + str(pageNumber), timeout = 3).read()
+            if scrapeFollowers:
+                followersPage = urllib2.urlopen("http://soundcloud.com/" + username + "/followers?page=" + str(pageNumber), timeout = 3).read()
+        except urllib2.HTTPError, e:
+            return 'User not found.'
+        except urllib2.URLError, e:
+            if pageNumber > 1:
+                break
+            else:
+                if isinstance(e.reason, socket.timeout):
+                    return 'Timed out.'
+                return 'Bummer, failed to connect :('
 
-		followingSoup = bs(followingPage).findAll("div", {"class" :"userbadge full"})
-		followersSoup = bs(followersPage).findAll("div", {"class" :"userbadge full"})
+        followingSoup = bs(followingPage).findAll("div", {"class" :"userbadge full"})
+        followersSoup = bs(followersPage).findAll("div", {"class" :"userbadge full"})
 
-		if (len(followingSoup) == 0):
-			scrapeFollowing = False
+        if (len(followingSoup) == 0):
+            scrapeFollowing = False
 
-		if (len(followersSoup) == 0):
-			scrapeFollowers = False
+        if (len(followersSoup) == 0):
+            scrapeFollowers = False
 
-		if scrapeFollowing == False and scrapeFollowers == False:
-			break
+        if scrapeFollowing == False and scrapeFollowers == False:
+            break
 
-		for following in followingSoup:
-			followingList[following.findAll(text=True)[0]] = following.find("a").get("href")
+        for following in followingSoup:
+            followingList[following.findAll(text=True)[0]] = following.find("a").get("href")
 
-		for followed in followersSoup:
-			followedList.append(followed.find("a").get("href"))
+        for followed in followersSoup:
+            followedList.append(followed.find("a").get("href"))
 
-		pageNumber = pageNumber + 1
+        pageNumber = pageNumber + 1
 
-	for followerName, eachFollower in followingList.items():
-		if eachFollower in followedList:
-			del followingList[followerName]
+    for followerName, eachFollower in followingList.items():
+        if eachFollower in followedList:
+            del followingList[followerName]
 
-	if len(followingList.keys()) == 0:
-		return 'Looks like circlejerks weren\'t pulled on this user.'
+    if len(followingList.keys()) == 0:
+        return 'Looks like circlejerks weren\'t pulled on this user.'
 
-	for followerName, eachFollower in followingList.items():
-		blackSheep = blackSheep + '<a href="http://soundcloud.com' + eachFollower + '/">' + followerName + '</a>' + '<br />'
-
-	return blackSheep
+    for followerName, eachFollower in followingList.items():
+        blackSheep = blackSheep + '<a href="http://soundcloud.com' + eachFollower + '/">' + followerName + '</a>' + '<br />'
+    
+    return blackSheep.encode('ascii', 'ignore')
 
 if __name__ == "__main__":
-	uname = str(raw_input("Enter your Soundcloud Username: "))
-	print(returnBlacklist(uname))
+    uname = str(raw_input("Enter your Soundcloud Username: "))
+    print(returnBlacklist(uname))
